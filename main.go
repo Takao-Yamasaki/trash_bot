37013ba8f37607f03a30131358d85b205472d9a8
山﨑 貴生 <yamasaki-ta@MACN0049.local>
2022-12-08 14:15:46 +0900

    HTTPメソッドの変更

--- main.go
+++ main.go
@@ -9,16 +9,13 @@ import (
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("view/*html")
-	// 一覧の取得
-	router.GET("/", controller.Index)
-	// データ1件取得
+	
+	router.GET("/trash-days", controller.Index)
 	router.GET("/trash-day/:id", controller.DetailsTrashDay)	
-	// 登録
 	router.POST("trash-day/create", controller.CreateTrashDay)
-	// 更新
-	router.PUT("/trash-day/update", controller.UpdateTrashDay)
-	// 削除
-	router.DELETE("/trash-day/delete", controller.DeleteTrashDay)
+	router.POST("/trash-day/update", controller.UpdateTrashDay)
+	router.POST("/trash-day/delete", controller.DeleteTrashDay)
+	
 	// サーバーの起動
 	router.Run(":8080")
 }
