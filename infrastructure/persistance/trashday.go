12ff8495894ec14ee04cc56de0689b92626532ea
山﨑 貴生 <yamasaki-ta@MACN0049.local>
2022-12-13 11:10:42 +0900

    メソットにエラーハンドリングを追加

--- infrastructure/persistance/trashday.go
+++ infrastructure/persistance/trashday.go
@@ -16,31 +16,53 @@ func NewTrashDayPersistance(conn *gorm.DB, c repository.TrashDayRepository) *tra
 }
 
 // １件の取得
-func (tr *trashDayPersistance) GetTrashDay(id int) model.TrashDay {
+func (tr *trashDayPersistance) GetTrashDay(id int) (result *model.TrashDay, err error) {
 	var td model.TrashDay
-	tr.Conn.First(&td, id)
+	if result := tr.Conn.First(&td, id); result.Error != nil {
+		err := result.Error
+		return nil, err
+	}
 
-	return td
+	return &td, nil
 }
 
 // 一覧の取得
-func (tr *trashDayPersistance) GetTrashDays() []model.TrashDay {
+func (tr *trashDayPersistance) GetTrashDays() (result []model.TrashDay, err error) {
 	var tds []model.TrashDay
-	tr.Conn.Find(&tds)
-	return tds
+	if result := tr.Conn.Find(&tds); result.Error != nil {
+		err := result.Error
+		return nil, err
+	}
+
+	return tds, nil
 }
 
 // 登録
-func (tr *trashDayPersistance) Create(td model.TrashDay) {
-	tr.Conn.Create(&td)
+func (tr *trashDayPersistance) Create(td model.TrashDay) error {
+	if result := tr.Conn.Create(&td); result.Error != nil {
+		err := result.Error
+		return err
+	}
+
+	return nil
 }
 
 // 更新
-func (tr *trashDayPersistance) Update(td model.TrashDay) {
-	tr.Conn.Save(&td)
+func (tr *trashDayPersistance) Update(td model.TrashDay) error {
+	if result := tr.Conn.Save(&td); result.Error != nil {
+		err := result.Error
+		return err
+	}
+
+	return nil
 }
 
 // 削除
-func (tr *trashDayPersistance) Delete(td model.TrashDay) {
-	tr.Conn.Delete(&td)
+func (tr *trashDayPersistance) Delete(td model.TrashDay) error {
+	if result := tr.Conn.Delete(&td); result.Error != nil {
+		err := result.Error
+		return err
+	}
+	
+	return nil
 }
