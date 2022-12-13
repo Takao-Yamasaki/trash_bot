2174478e4a153576956a0a23b3b641cbfbb7b9b4
山﨑 貴生 <yamasaki-ta@MACN0049.local>
2022-12-13 14:08:23 +0900

    controllerでのエラーハンドリング

--- controller/trashday.go
+++ controller/trashday.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"strconv"
 	"trash_bot/domain/model"
 	"trash_bot/domain/repository"
@@ -8,7 +9,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type trashDayController struct{
+type trashDayController struct {
 	trashDayRepository repository.TrashDayRepository
 }
 
@@ -20,21 +21,49 @@ func NewTrashDayController(tr repository.TrashDayRepository) trashDayController 
 
 // 一覧の取得
 func (tc *trashDayController) IndexTrashDay(c *gin.Context) {
-	tds := tc.trashDayRepository.GetTrashDays()
+	tds, err := tc.trashDayRepository.GetTrashDays()
+	if err != nil {
+		fmt.Println(err)
+		c.HTML(500, "500.html", gin.H{"error": err.Error()})
+		return
+	}
 	c.HTML(200, "trashday/index.html", gin.H{"tds": tds})
 }
 
 // 詳細の取得
 func (tc *trashDayController) DetailTrashDay(c *gin.Context) {
-	id, _ := strconv.Atoi(c.Param("id"))
-	td := tc.trashDayRepository.GetTrashDay(id)
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		fmt.Println(err)
+		c.HTML(400, "400.html", gin.H{"error": err.Error()})
+		return
+	}
+	td, err := tc.trashDayRepository.GetTrashDay(id)
+	if err != nil {
+		fmt.Println(err)
+		c.HTML(500, "500.html", gin.H{"error": err.Error()})
+		return
+	}
 	c.HTML(200, "trashday/detail.html", gin.H{"td": td})
 }
 
 // 登録
 func (tc *trashDayController) CreateTrashDay(c *gin.Context) {
-	day := c.PostForm("day")
-	trash := c.PostForm("trash")
+	type RequestDataField struct {
+		Day string `form:"day" binding:"required"`
+		Trash string `form:"trash" binding:"required"`
+	}
+
+	var form RequestDataField
+
+	if err := c.ShouldBind(&form); err != nil {
+		fmt.Println(err)
+		c.HTML(400, "400.html", gin.H{"error": err.Error()})
+	}
+
+	day := form.Day
+	trash := form.Trash
+
 	td := model.TrashDay{Day: day, Trash: trash}
 	tc.trashDayRepository.Create(td)
 
@@ -43,23 +72,83 @@ func (tc *trashDayController) CreateTrashDay(c *gin.Context) {
 
 // 更新
 func (tc *trashDayController) UpdateTrashDay(c *gin.Context) {
-	id, _ := strconv.Atoi(c.PostForm("id"))
-	td := tc.trashDayRepository.GetTrashDay(id)
 	
-	day := c.PostForm("day")
-	trash := c.PostForm("trash")
+	type RequestDataField struct {
+		ID string `form:"id" binding:"required"`
+		Day string	`form:"day" binding:"required"`
+		Trash string `form:"trash" binding:"required"`
+	}
+	
+	var form RequestDataField
+	
+	if err := c.ShouldBind(&form); err != nil {
+		fmt.Println(err)
+		c.HTML(400, "400.html", gin.H{"error": err.Error()})
+		return
+	}
+
+	id, err := strconv.Atoi(form.ID)
+	if err != nil {
+		c.HTML(400, "400.html", gin.H{"error": err.Error()})
+		return
+	}
+
+	day := form.Day
+	trash := form.Trash
+
+	td, err := tc.trashDayRepository.GetTrashDay(id)
+	if err != nil {
+		fmt.Println(err)
+		c.HTML(500, "500.html", gin.H{"error": err.Error()})
+		return
+	}
+	
 	td.Day = day
 	td.Trash = trash
-	tc.trashDayRepository.Update(td)
+	err = tc.trashDayRepository.Update(*td)
+	if err != nil {
+		fmt.Println(err)
+		c.HTML(500, "500.html", gin.H{"error": err.Error()})
+		return
+	}
 
 	c.Redirect(301, "/trash-day/index")
 }
 
 // 削除
 func (tc *trashDayController) DeleteTrashDay(c *gin.Context) {
-	id, _ := strconv.Atoi(c.PostForm("id"))
-	td := tc.trashDayRepository.GetTrashDay(id)
-	tc.trashDayRepository.Delete(td)
+	type RequestDataField struct {
+		ID string `form:"id" binging:"required"`
+	}
+
+	var form RequestDataField
+	
+	if err := c.ShouldBind(&form); err != nil {
+		fmt.Println(err)
+		c.HTML(400, "400.html", gin.H{"error": err.Error()})
+		return
+	}
+
+	id, err := strconv.Atoi(form.ID)
+	if err != nil {
+		fmt.Println()
+		c.HTML(400, "400.html", gin.H{"error": err.Error()})
+		return
+	}
+
+	td, err := tc.trashDayRepository.GetTrashDay(id)
+	if err != nil {
+		fmt.Println(err)
+		c.HTML(500, "500.html", gin.H{"error": err.Error()})
+		return
+	}
+	
+	err = tc.trashDayRepository.Delete(*td)
+	if err != nil {
+		fmt.Println(err)
+		c.HTML(500, "500.html", gin.H{"error": err.Error()})
+		return
+	}
 
 	c.Redirect(301, "/trash-day/index")
 }
